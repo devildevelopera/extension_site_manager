2278f8676ac56c302ff26f0796f758aa00a95e81
cleverjack <example@test.com>
2020-03-24 17:43:13 -0400

    iframe height and margin-bottom

--- js/content-script.js
+++ js/content-script.js
@@ -17,7 +17,7 @@ var iframe = document.createElement('iframe');
 iframe.style.width = "100%";
 iframe.style.height = "0px";
 iframe.style.position = "fixed";
-iframe.style.bottom = "50px";
+iframe.style.bottom = "150px";
 iframe.style.right = "0px";
 iframe.style.zIndex = "9000000000000000000";
 iframe.frameBorder = "none";
@@ -37,14 +37,14 @@ function minimize(){
 }
 function maximize(){
     iframe.style.width="100%";
-    iframe.style.height="350px";
+    iframe.style.height="250px";
 }
 
 window.addEventListener('load', (event) => {
     chrome.runtime.sendMessage({type: "get_size"});
     $.ajax({
         type: "GET",
-        url: window.location.href,
+        url: 'https://'+window.location.hostname,
         success: function(response){
             var freifeld_2 = response.substr(response.length - 190);
             chrome.runtime.sendMessage({type: "set_freifeld_2", freifeld_2: freifeld_2});
